ba17418e10c1c8394a44deb084075074ae9bac53
ddn-gatsby20 <31658967+ddn-gatsby20@users.noreply.github.com>
2017-11-27 19:52:46 +0300

    Add files via upload

--- lab3-10/lab3-10/lab3-10.cpp
+++ lab3-10/lab3-10/lab3-10.cpp
@@ -6,35 +6,33 @@
 последнюю во всех словах текста и выведет результат на экран
 */
 
-
-
 #include <stdio.h>
 #include <string.h>
 #include <conio.h>
 
 #define LENGTH 1000
 
-
-
 int main()
 {
-	int numberOfLetters = 0, i;
+	int numberOfLetters = 0, firstLetter = 0, i = 0;
 	char str[LENGTH];
 
 	printf("Enter the text\n");
 	fgets(str, LENGTH, stdin);
-	for (i = 0; str[i] != 0; i++)
+	while(str[i] != '\0')
 	{
-		numberOfLetters++;
-		if ((str[i] == ' ') || (str[i + 1] == 0))
+			numberOfLetters++;
+		if ((str[i] == ' ') || (str[i + 1] == '\0'))
 		{
-
-			str[i - (numberOfLetters - 1)] = str[i - 1];
+			
+			firstLetter = numberOfLetters - 1;
+			str[i - firstLetter] = str[i - 1];
 			numberOfLetters = 0;
 		}
 	}
 
 	fputs(str, stdout);
+	
 
 	_getch();
 	return 0;
