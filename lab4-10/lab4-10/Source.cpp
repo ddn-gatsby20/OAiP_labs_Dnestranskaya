560bc0522f19b1aa510b60abf15bb74603f3045d
ddn-gatsby20 <31658967+ddn-gatsby20@users.noreply.github.com>
2017-11-30 00:06:50 +0300

    Add files via upload

---
+++ lab4-10/lab4-10/Source.cpp
@@ -0,0 +1,66 @@
+/*
+Днетранская Дарья Сергеевна
+Лабороторная работа №4
+Вариант 10
+Задание:Написать программу, которая в вводимом с клавиатуры тексте заменить первую букву на
+последнюю во всех словах текста и выведет результат на экран
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+
+#define LENGTH 1000
+
+int array(int end, char *str, FILE *file);
+
+int main()
+{
+	int numberOfLetters = 0, firstLetter = 0, i = 0, end = 0;;
+	char str[LENGTH];
+
+	FILE *file1 = fopen("file1.txt", "r");
+
+	if (file1 != NULL)
+	{
+		FILE *newFile = fopen("file2.txt", "w");
+		for (int i = 0; fgetc(file1) != EOF; i++)
+			end++;
+		fclose(file1);
+		FILE *file = fopen("file1.txt", "r");
+	
+		if (file != NULL)
+		{
+			array(end, str, file);
+
+			for (int i = 0; str[i] != '\0'; i++)
+			{
+				numberOfLetters++;
+				if ((str[i] == ' ') || (str[i + 1] == '\0'))
+				{
+
+					firstLetter = numberOfLetters - 1;
+					str[i - firstLetter] = str[i - 1];
+					numberOfLetters = 0;
+				}
+			}
+			for (int i = 0; i < end; i++)
+				fprintf(newFile, "%c", str[i]);
+			fclose(newFile);
+			fclose(file);
+		}
+		else printf("File not found");
+	}
+	else printf("File not found");
+	_getch();
+	return 0;
+}
+
+
+int array(int end, char *str, FILE *file)
+	{
+	for (int i = 0; i < end; i++)
+		str[i] = fgetc(file);
+	return 0;
+	}
