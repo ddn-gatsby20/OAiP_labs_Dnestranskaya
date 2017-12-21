d3098448cff00c1044dbd8cfc14e7f30181089d1
ddn-gatsby20 <31658967+ddn-gatsby20@users.noreply.github.com>
2017-12-21 20:09:18 +0300

    Add files via upload
    Исправлены кодировка, define, вывод вычисленных значений

---
+++ lab1-1/lab1-1/lab1-1.cpp
@@ -0,0 +1,34 @@
+﻿/* Днестранская Дарья Сергеевна
+Лабораторная работа №1
+Вариант №1
+Задание: Перевести версты в сажени и сантиметры
+*/
+
+#define _CRT_SECURE_NO_WARNINGS 
+#define SAJENI 583
+#define CENTIMETRES 106680
+
+#include <stdio.h>
+#include <locale.h>
+#include <conio.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	float verst;
+	printf("Введите меру в верстах   ");
+	int result = scanf("%f", &verst);
+	if (result == 0)
+	{
+		printf("Некорректный ввод данных");
+	}
+	else
+	{
+		float vsajeni = verst * SAJENI;
+		float vcentimeters = verst * CENTIMETRES;
+		printf("\n Мера в саженях %.1f ", vsajeni);
+		printf("\n Мера в сантиметрах %.1f \n  ", vcentimeters);
+	}
+	_getch();
+	return 0;
+}
