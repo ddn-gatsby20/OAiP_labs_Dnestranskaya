f849ed2a257990f326c0cd4a7f8062fc8962d5c4
ddn-gatsby20 <31658967+ddn-gatsby20@users.noreply.github.com>
2017-12-22 21:11:33 +0300

    Полностью переделан код

--- lab3-10/lab3-10/lab3-10.cpp
+++ lab3-10/lab3-10/lab3-10.cpp
@@ -1,6 +1,6 @@
 ﻿/*
 Днетранская Дарья Сергеевна
-Лабороторная работа №3
+Лабораторная работа №3
 Вариант 10
 Задание:Написать программу, которая в вводимом с клавиатуры тексте заменит первую букву на
 последнюю во всех словах текста и выведет результат на экран
@@ -10,30 +10,34 @@
 #include <string.h>
 #include <conio.h>
 
+
 #define LENGTH 1000
+
+void replacement(char *m);
 
 int main()
 {
-	int numberOfLetters = 0, firstLetter = 0, i = 0;
 	char str[LENGTH];
-
 	printf("Enter the text\n");
 	fgets(str, LENGTH, stdin);
-	while(str[i] != '\0')
-	{
-			numberOfLetters++;
-		if ((str[i] == ' ') || (str[i + 1] == '\0'))
-		{
-			
-			firstLetter = numberOfLetters - 1;
-			str[i - firstLetter] = str[i - 1];
-			numberOfLetters = 0;
-		}
-	}
-
+	replacement(str);
 	fputs(str, stdout);
-	
-
 	_getch();
 	return 0;
+}
+
+void replacement(char *m)
+{
+	int  firstLetter = 0;
+	for (int i = 1; m[i] != '\0'; i++)
+	{
+		if (m[i] == ' ')
+		{
+			firstLetter = i + 1;
+		}
+		if ((m[i + 1] == ' ' || m[i + 1] == '\n') && m[firstLetter] != ' ')
+		{
+			m[firstLetter] = m[i];
+		}
+	}
 }
